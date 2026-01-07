24ce9196e94a4c048979ebdcc5b4df1a6438f230
Mohamed ID-ABDELLAH <mohamed.id-abdellah@outlook.com>
2026-01-08 00:06:53 +0100

    new

--- Data Structures/05 - BST.ts
+++ Data Structures/05 - BST.ts
@@ -1,13 +1,16 @@
 import { Queue } from "./04 - Queue"
 
-class BSTNode<G> {
-    public right: BSTNode<G> | null
-    public left: BSTNode<G> | null
-    public value: G
-    constructor(v: G) {
-        this.right = null
-        this.left = null
-        this.value = v
+class BSTNode<T> {
+    public value: T
+    public right: BSTNode<T> | null
+    public left: BSTNode<T> | null
+    public parent: BSTNode<T> | null
+
+    constructor(value: T) {
+        this.value = value
+        this.right = null;
+        this.left = null;
+        this.parent = null
     }
 }
 
@@ -23,156 +26,151 @@ class BinarySearchTree<T> {
         if (node && node.right) {
             this.print(node.right, prefix + (isLeft ? "│   " : "    "), false);
         }
-
         console.log(prefix + (isLeft ? "└── " : "┌── ") + node!.value);
-
         if (node && node.left) {
             this.print(node.left, prefix + (isLeft ? "    " : "│   "), true);
         }
     }
 
+    public insert(value: T): boolean {
+        const newNode = new BSTNode<T>(value)
 
-    public insert(v: T) {
-        const newNode = new BSTNode<T>(v)
-        if (!this.root) {
+        if (this.root == null) {
             this.root = newNode
-            return this
+            return true
         }
 
         let current = this.root
-        while (current) {
-            if (current.value === newNode.value) return null
-            if (newNode.value > current.value) {
-                if (current.right) {
-                    current = current.right
-                } else {
+
+        while (true) {
+            if (value > current.value) {
+                if (current.right == null) {
                     current.right = newNode
-                    return this
-                }
-            } else if (newNode.value < current.value) {
-                if (current.left) {
-                    current = current.left
+                    current.right.parent = current
+                    return true
                 } else {
-                    current.left = newNode
-                    return this
+                    current = current.right
                 }
+            } else if (value < current.value) {
+                if (current.left == null) {
+                    current.left = newNode
+                    current.left.parent = current
+                    return true
+                } else {
+                    current = current.left
+                }
+            } else {
+                return false
             }
         }
     }
 
-    public find(v: T): BSTNode<T> | null {
-        if (!this.root) return null
-
-        let current: BSTNode<T> | null = this.root
-        while (current) {
-            if (current.value === v) return current;
-            if (v > current.value) {
-                current = current.right
-            } else if (v < current.value) {
-                current = current.left
-            }
-        }
-
-        return null
-    }
-
-    public contains(v: T) {
-        return !!this.find(v)
-    }
-
-    private _getParent(node: BSTNode<T>): BSTNode<T> | null {
-        if (!this.root || this.root === node) return null;
+    public contains(value: T): boolean {
+        if (this.root == null) return false
         let current: BSTNode<T> | null = this.root
 
         while (current) {
-            if (current.left === node || current.right === node) return current
-            if (node.value > current.value) {
+            if (value == current.value) return true
+            if (value > current.value) {
                 current = current.right
-            } else if (node.value < current.value) {
+            } else if (value < current.value) {
                 current = current.left
             }
         }
-
-        return null
+        return false
     }
 
-    public BFS(returnNodes?: boolean) {
+
+    public min(): T | null {
+        if (this.root == null) return null
+        let current: BSTNode<T> = this.root
+        while (current.left != null) {
+            current = current.left
+        }
+        return current.value
+    }
+
+    public max(): T | null {
+        if (this.root == null) return null
+        let current: BSTNode<T> = this.root
+        while (current.right != null) {
+            current = current.right
+        }
+        return current.value
+    }
+
+    /* Traversal */
+
+    public inOrder(): T[] {
         if (!this.root) return []
+        let visited: T[] = [];
+        let current = this.root
+        function traverse(node: BSTNode<T> | null) {
+            if (node && node.left !== null) traverse(node.left)
+            if (node) visited.push(node.value)
+            if (node && node.right !== null) traverse(node.right)
+        }
+        traverse(current)
+        return visited;
+    }
 
-        let q = new Queue<BSTNode<T>>()
-        q.enqueue(this.root)
+    public preOrder(): T[] {
+        if (!this.root) return []
+        let visited: T[] = [];
+        let current = this.root
+        function traverse(node: BSTNode<T> | null) {
+            if (node) visited.push(node.value)
+            if (node && node.left !== null) traverse(node.left)
+            if (node && node.right !== null) traverse(node.right)
+        }
+        traverse(current)
+        return visited;
+    }
 
-        let visited: (BSTNode<T> | T)[] = []
+    public postOrder(): T[] {
+        if (!this.root) return []
+        let visited: T[] = [];
+        let current = this.root
+        function traverse(node: BSTNode<T> | null) {
+            if (node && node.left !== null) traverse(node.left)
+            if (node && node.right !== null) traverse(node.right)
+            if (node) visited.push(node.value)
+        }
+        traverse(current)
+        return visited;
+    }
 
-        while (q.size !== 0) {
-            let dequeued = q.dequeue()!
-            visited.push(returnNodes ? dequeued : dequeued.value)
-            if (dequeued.left) q.enqueue(dequeued.left)
-            if (dequeued.right) q.enqueue(dequeued.right)
+    public levelOrder(): T[] {
+        if (!this.root) return [];
+        let queue: Queue<BSTNode<T>> = new Queue<BSTNode<T>>();
+        queue.enqueue(this.root);
+        let visited: T[] = [];
+        while (queue.size !== 0) {
+            let firstItem: BSTNode<T> = queue.dequeue() as BSTNode<T>
+            visited.push(firstItem.value)
+            if (firstItem.left) queue.enqueue(firstItem.left)
+            if (firstItem.right) queue.enqueue(firstItem.right)
         }
         return visited
     }
-
-    public DFS(which: "inorder" | "preorder" | "postorder", returnNodes?: boolean): (BSTNode<T> | T)[] {
-        return {
-            "inorder": () => {
-                if (!this.root) return [];
-
-                let visited: (BSTNode<T> | T)[] = [];
-                let current = this.root
-
-                function traverse(node: BSTNode<T> | null) {
-                    if (!node) return
-                    if (node.left) traverse(node.left)
-                    visited.push(returnNodes ? node : node.value)
-                    if (node.right) traverse(node.right)
-                }
-                traverse(current)
-                return visited
-            },
-            "preorder": () => {
-                if (!this.root) return [];
-
-                let visited: (BSTNode<T> | T)[] = [];
-                let current = this.root
-
-                function traverse(node: BSTNode<T> | null) {
-                    if (!node) return
-                    visited.push(returnNodes ? node : node.value)
-                    if (node.left) traverse(node.left)
-                    if (node.right) traverse(node.right)
-                }
-                traverse(current)
-                return visited
-            },
-            "postorder": () => {
-                if (!this.root) return [];
-
-                let visited: (BSTNode<T> | T)[] = [];
-                let current = this.root
-
-                function traverse(node: BSTNode<T> | null) {
-                    if (!node) return
-                    if (node.left) traverse(node.left)
-                    if (node.right) traverse(node.right)
-                    visited.push(returnNodes ? node : node.value)
-                }
-                traverse(current)
-                return visited
-            }
-        }[which]()
-    }
-
 }
 
 
 const bst = new BinarySearchTree<number>();
 
-[50, 17, 72, 12, 23, 54, 76, 9, 14, 19, 67, 80, 51].forEach(e => bst.insert(e))
+bst.insert(10)
+bst.insert(14)
+bst.insert(8)
+bst.insert(9)
+bst.insert(7)
+bst.insert(12)
+bst.insert(13)
+bst.insert(11)
+bst.insert(15)
+
+// console.log("in order:", bst.inOrder())
+// console.log("pre order:", bst.preOrder())
+// console.log("post order:", bst.postOrder())
+// console.log("level order:", bst.levelOrder())
 
 bst.print()
-
-console.log("BFS", bst.BFS())
-console.log("DFS", bst.DFS("inorder"))
-console.log("DFS", bst.DFS("preorder"))
-console.log("DFS", bst.DFS("postorder"))
